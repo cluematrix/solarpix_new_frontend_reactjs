d42d8d0585a8ef13659346538935881846fff725
RishirajKorde <newemail@example.com>
2025-08-29 14:48:16 +0530

    env create

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "http://192.168.1.14:3000", // Base URL for Locally API
-    baseURL: "http://20.193.154.1:3000", // Base URL for Live API
+  baseURL: process.env.REACT_APP_BASE_URL, // Base URL for API
 
   headers: {
     "Content-Type": "application/json",
